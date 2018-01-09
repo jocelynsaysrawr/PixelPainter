218bf7406e2db7f7949e20dfafddc05c927d3f69
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-01-09 00:11:14 -1000

    generate color picker

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -15,7 +15,31 @@ function pixelPainterModule() {
             }
             ppCanvas.appendChild(cols);
         }
-  
+    }
+
+    var allColors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","WhiteSmoke","Yellow","YellowGreen"];
+        
+    function colorGrid() {
+        var colorCanvas = document.createElement('div');
+        colorCanvas.id = 'colorCanvas';
+        pixelPainter.appendChild(colorCanvas);
+
+        var colorIndex = 0;
+
+        for (var i = 0; i < 12; i++) {
+            var cols2 = document.createElement('div');
+            cols2.className = 'color';
+            for (var j = 1; j <= 12; j++, colorIndex++) {
+                var colorCell = document.createElement('div');
+                colorCell.className = 'colorCell';
+                colorCell.style.backgroundColor = allColors[colorIndex];
+                cols2.appendChild(colorCell);
+            }
+            colorCanvas.appendChild(cols2);
+        }
+    }
+
+
             
         
 
@@ -23,7 +47,7 @@ function pixelPainterModule() {
 
 
      
-    }
+    
 
 
 
@@ -34,10 +58,13 @@ function pixelPainterModule() {
 
 
     return {
-        PixelPainter: PixelPainter
+        PixelPainter: PixelPainter,
+        colorGrid: colorGrid
     };
 
 }
 
 var myPixelPainter = pixelPainterModule();
 myPixelPainter.PixelPainter(25, 30);
+myPixelPainter.colorGrid();
+
